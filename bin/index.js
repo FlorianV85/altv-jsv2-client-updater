389929b85cba105fba77f59908335cca5e4ef6f4
FlorianV85 <15657365+FlorianV85@users.noreply.github.com>
2023-12-31 17:16:49 +0100

    fix: improve code

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,13 @@ import chalk from 'chalk';
 import decompress from 'decompress';
 import path from "path";
 
-const configFile = '.jsv2-client-updater.json';
-const tempFolderPath = './.temp'
-const tempFile = `${tempFolderPath}/temp.zip`;
+const configFile = path.resolve('.jsv2-client-updater.json');
+const tempFolderPath = path.resolve('./.temp')
+const tempFile = path.resolve(tempFolderPath, 'temp.zip');
 
 async function createJsonConfigFile() {
     try {
-        const defaultContent = JSON.stringify({ altVPath: '' }, null, 4);
+        const defaultContent = JSON.stringify({altVPath: ''}, null, 4);
 
         await fs.writeFile(`./${configFile}`, defaultContent, 'utf8');
 
@@ -18,13 +18,34 @@ async function createJsonConfigFile() {
             ${chalk.green('A configuration file has been created.')}
             ${chalk.yellow('Please modify it with the path to the root of your alt:V launcher folder and re-execute this script.')}
         `);
-    } catch(e) {console.error('Unable to create configuration file', e);
+    } catch (e) {
+        console.error('Unable to create configuration file', e);
+    }
+}
+
+async function getAltVFolderPath() {
+    try {
+        const {altVPath} = JSON.parse(await fs.readFile(configFile, 'utf8'));
+
+        if (!altVPath) {
+            console.error(chalk.red(`Please add the path to your alt:V launcher folder in the ${configFile}.`));
+            return;
+        }
+
+        return altVPath;
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            await createJsonConfigFile();
+            return;
+        }
+
+        console.error('Error reading configuration file', e);
     }
 }
 
 async function getModuleVersion() {
     const response = await fetch('https://cdn.alt-mp.com/js-module-v2/dev/x64_win32/update.json', {
-        headers: { "Accept": "application/json" }
+        headers: {Accept: "application/json"}
     });
 
     const versionFile = await response.json();
@@ -37,11 +58,11 @@ async function downloadClientModuleFiles(version) {
 
     const response = await fetch(githubDownloadURL);
 
-    if(!response.ok) {
+    if (!response.ok) {
         throw new Error(`Error downloading client module: ${response.statusText}`);
     }
 
-    await fs.mkdir(tempFolderPath, { recursive: true });
+    await fs.mkdir(tempFolderPath, {recursive: true});
 
     await fs.writeFile(tempFile, Buffer.from(await response.arrayBuffer()));
 
@@ -53,9 +74,9 @@ async function extractClientModuleFiles() {
 
 async function copyFilesToAltVLauncher(altVPath) {
     const files = [`${tempFolderPath}/dist-client-windows/modules/js-client-v2.dll`, `${tempFolderPath}/dist-client-windows/modules/js-client-v2.pdb`];
-    const altVModulePath = path.join(altVPath, '/modules/js-client-v2');
+    const altVModulePath = path.resolve(altVPath, 'modules', 'js-client-v2');
 
-    await fs.mkdir(altVModulePath, { recursive: true });
+    await fs.mkdir(altVModulePath, {recursive: true});
 
     await Promise.all(files.map(file => fs.copyFile(file, path.join(altVModulePath, path.basename(file)))));
 }
@@ -67,50 +88,38 @@ async function deleteTempFiles() {
 
     const isOtherFilesInTempFolder = filesInTempFolder.some(file => !targetFiles.includes(file));
 
-    if(isOtherFilesInTempFolder) {
-        const deletePromises = targetFiles.map(file => fs.rm(path.join(tempFolderPath, file), { recursive: true }));
+    if (isOtherFilesInTempFolder) {
+        const deletePromises = targetFiles.map(file => fs.rm(path.join(tempFolderPath, file), {recursive: true}));
         await Promise.allSettled(deletePromises);
     } else {
-        await fs.rm(tempFolderPath, { recursive: true });
+        await fs.rm(tempFolderPath, {recursive: true});
     }
 }
 
 async function update() {
+    const altVPath = await getAltVFolderPath();
+
+    if (!altVPath) return;
+
     try {
-        const { altVPath } = JSON.parse(await fs.readFile(`./${configFile}`, 'utf8'));
+        await fs.access(path.join(altVPath, 'altv.exe'));
+    } catch (e) {
+        console.error(chalk.red('The path indicated in the configuration file is not the alt:V launcher folder'));
+        return;
+    }
 
-        if(!altVPath) {
-            console.error(chalk.red('Please add the path to your alt:V launcher folder in the ${configFile}.'));
-            return;
-        }
+    try {
+        const version = await getModuleVersion();
 
-        const altVFolderFiles = await fs.readdir(altVPath);
+        if (!version) throw new Error('The fetched version is undefined');
 
-        if(!altVFolderFiles.includes('altv.exe')) {
-            console.error(chalk.red('The path indicated in the configuration file is not the alt:V launcher folder'));
-            return;
-        }
+        await downloadClientModuleFiles(version);
+        await extractClientModuleFiles();
+        await copyFilesToAltVLauncher(altVPath);
 
-        try {
-            const version = await getModuleVersion();
-
-            if(!version) throw new Error('The fetched version is undefined');
-
-            await downloadClientModuleFiles(version);
-            await extractClientModuleFiles();
-            await copyFilesToAltVLauncher(altVPath);
-
-            console.log(chalk.green(`JS V2 client module successfully updated to version ${version}`));
-        } catch(e) {
-            console.error(e);
-        }
-    } catch(e) {
-        if(e.code === 'ENOENT') {
-            await createJsonConfigFile();
-            return;
-        }
-
-        console.error('Error reading configuration file', e);
+        console.log(chalk.green(`JS V2 client module successfully updated to version ${version}`));
+    } catch (e) {
+        console.error(e);
     }
 
     await deleteTempFiles()
